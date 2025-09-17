edbb3280a7787c2d41218790000aa47047f32dd8
Ahmad Al Fayad Chowdhury <fayad.chowdhury@gmail.com>
2025-09-17 01:29:18 -0700

    Modify pipeline to reuse embeddings and existing vector store first

--- experiments/run_experiment.py
+++ experiments/run_experiment.py
@@ -11,7 +11,11 @@ from core.utils import (
     initialize_generator,
     parser_router,
     get_env_config,
-    read_prompt
+    read_prompt,
+    check_for_embeddings,
+    save_embeddings,
+    load_embeddings,
+    save_config_yaml
 )
 
 if __name__=="__main__":
@@ -49,31 +53,55 @@ if __name__=="__main__":
 
     # Read prompts
     prompts_base_dir = f"experiments/prompts/{experiment_name}"
-    prompt_template = read_prompt(prompts_base_dir, "qa")
-    system_prompt = read_prompt(prompts_base_dir, "system")
+    prompt_template = read_prompt(prompts_base_dir, generator_config["config"]["qa_prompt_path"])
+    system_prompt = read_prompt(prompts_base_dir, generator_config["config"]["system_prompt_path"])
     
     # Initialize generator
     generator_config["config"]["api_key"] = env_config.get(generator_config["config"].get("api_key", ""), "")
     generator = initialize_generator(system_prompt, prompt_template, generator_config)
 
-    # Loop over data files
-    data_path = Path(experiment_config["data"])
-    for file_path in data_path.rglob("*"):
-        if file_path.is_file():
-            file_path = str(file_path)
-            print(f"Working on: {file_path}")
-            parser = parser_router(all_parsers, file_path)
-            parsed_document = parser.parse(file_path)
-            print(f"Finished parsing")
-            print(f"Starting chunking")
-            chunks = chunker.chunk(parsed_document)
-            print(f"Finished chunking")
-            print(f"Starting embedding")
-            embeddings = embedder.embed_data(chunks)
-            print(f"Finished embedding")
-            print(f"Pushing to vector store")
-            vector_store.store_batch(embeddings, batch_size=10)
-            print(f"Finished pushing to vector store")
+    # Check to see if database insertion logged
+    if not experiment_config.get("vector_store_exists", False):
+        print(f"Vector store does not exist")
+        # Check to see if embeddings exist already
+        if not check_for_embeddings(experiment_config["embeddings_dir"]):
+            print(f"Embeddings not found")
+            # Loop over data files
+            embeddings_to_save = []
+            data_path = Path(experiment_config["data"])
+            for file_path in data_path.rglob("*"):
+                if file_path.is_file():
+                    file_path = str(file_path)
+                    print(f"Working on: {file_path}")
+                    parser = parser_router(all_parsers, file_path)
+                    parsed_document = parser.parse(file_path)
+                    print(f"Finished parsing")
+                    print(f"Starting chunking")
+                    chunks = chunker.chunk(parsed_document)
+                    print(f"Finished chunking")
+                    print(f"Starting embedding")
+                    embeddings = embedder.embed_data(chunks)
+                    embeddings_to_save += [embedding for embedding in embeddings]
+                    print(f"Finished embedding")
+                    
+            
+            print(f"Saving embeddings")
+            save_embeddings(embeddings_to_save, experiment_config["embeddings_dir"])
+            experiment_config["embeddings_saved"] = True
+            save_config_yaml(experiment_config, configs_base_dir, "experiment")
+        else:
+            print(f"Embeddings found")
+            embeddings = load_embeddings(experiment_config["embeddings_dir"])
+            print(f"Loaded embeddings")
+
+        # Save
+        print(f"Pushing to vector store")
+        vector_store.store_batch(embeddings, batch_size=10)
+        print(f"Finished pushing to vector store")
+        experiment_config["vector_store_exists"] = True
+        save_config_yaml(experiment_config, configs_base_dir, "experiment")
+    else:
+        print(f"Vector store exists")
     
     # Get queries
     query_pass = experiment_config["query_pass"]
