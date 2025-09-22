b777f71528d03d83ced91d219accf2748f21c6b9
Ahmad Al Fayad Chowdhury <fayad.chowdhury@gmail.com>
2025-09-22 13:08:08 -0700

    Also return doc names in retreiver

--- core/retrievers/TopKRetriever.py
+++ core/retrievers/TopKRetriever.py
@@ -6,11 +6,14 @@ class TopKRetriever(BaseRetriever):
         self.k = self.config.get("top_k", 10)
 
     def retrieve(self, query):
-        return_docs = []
+        return_doc_names = []
+        return_doc_texts = []
         self.query_embedding = self.embedder.embed_text(query)
         query_docs = self.vector_store.query_top_k(self.query_embedding, self.k)
         for doc in query_docs:
             text = doc["metadata"]["text"]
-            return_docs.append(text)
+            doc_name = doc["metadata"]["file_name"]
+            return_doc_names.append(doc_name)
+            return_doc_texts.append(text)
 
-        return return_docs
+        return return_doc_names, return_doc_texts
