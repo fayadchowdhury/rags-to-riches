bb2c5b409cddc7841d8c24bdcce9fa8dbcd776b4
Ahmad Al Fayad Chowdhury <fayad.chowdhury@gmail.com>
2025-09-29 22:57:26 -0700

    Render the first 100 characters of the content

--- ui/components/Source.py
+++ ui/components/Source.py
@@ -6,4 +6,4 @@ class Source:
         self.source = source
 
     def render(self):
-        st.markdown(f"- {self.source['title']}: {self.source['content']}")
+        st.markdown(f"- {self.source['title']}: {self.source['content'][:100] + "..."}")
